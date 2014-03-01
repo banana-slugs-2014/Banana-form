894079556090a079fe8cb935ac7e489fd5379a9e
Mathilde Mouw <apprentice@DBC-Infinity-4.local>
2014-03-01 01:49:50 -0800

    added beggining of b crypt authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,22 @@ class User < ActiveRecord::Base
   has_many :completed_surveys
   has_many :surveys
   has_many :responses
+
+  require 'bcrypt'
+
+  class User < ActiveRecord::Base
+    # users.password_hash in the database is a :string
+    include BCrypt
+
+    def password
+      @password ||= Password.new(password_hash)
+    end
+
+    def password=(new_password)
+      @password = Password.create(new_password)
+      self.password_hash = @password
+    end
+
+  end
+
 end
