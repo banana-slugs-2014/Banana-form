ccd82d66059f6e1a8f447f91ad9ee81a8cafd188
hpchess <hpchess@gmail.com>
2014-03-02 12:51:39 -0800

    add user must be logged in to create or take a survey.
    reason: The user should not be able to create or take surveys when
    logged in.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,7 +13,11 @@ post '/surveys' do
 end
 
 get '/surveys/new' do
-  erb :create
+  if logged_in?
+    erb :create
+  else
+    redirect "/login"
+  end
 end
 
 get '/surveys/:id' do
@@ -24,20 +28,26 @@ end
 
 
 get '/surveys/take/:id' do
-  @survey = Survey.find( params[:id] )
-  @question = @survey.questions.includes(:choices)
-  erb :take_survey
+  if logged_in?
+    @survey = Survey.find( params[:id] )
+    @question = @survey.questions.includes(:choices)
+    erb :take_survey
+  else
+    redirect "/login"
+  end
 end
 
 post '/surveys/take' do
-  user = User.find(session[:user_id])
-  params.each_value do |value|
-    Response.create( choice_id: value, user_id: user.id)
+  if logged_in?
+    user = User.find(session[:user_id])
+    params.each_value do |value|
+      Response.create( choice_id: value, user_id: user.id)
+    end
+    redirect '/'
+  else
+    redirect '/login'
   end
-
-  redirect '/'
 end
-
 
 get '/questions/:id/new' do
   @survey_id = params[:id]
@@ -45,11 +55,11 @@ get '/questions/:id/new' do
 end
 
 post '/questions/:survey_id' do
-  @question = Question.create!(survey_id: params[:survey_id], question_text: params[:question_text])
+  @question = Question.create!(survey_id: params[:survey_id],
+    question_text: params[:question_text])
   @question.choices.create(choice_text: params[:choice_1])
   @question.choices.create(choice_text: params[:choice_2])
   @question.choices.create(choice_text: params[:choice_3])
   @question.choices.create(choice_text: params[:choice_4])
   redirect "/questions/#{params[:survey_id]}/new"
 end
-
