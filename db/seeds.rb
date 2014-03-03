6d006ee9d50a8b7ecef4b2ff4f650d1888352f3f
hpchess <hpchess@gmail.com>
2014-03-03 00:52:59 -0800

    Add sign up link to navbar. Increase db seed size.
    reason: there was no sign up link.
    The seed size was too small for meaningfull history page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,17 @@ tom = User.create(username: "tom", password: "123")
 
 firstpost = bob.surveys.create(title: "Test Survey")
 monkey = bob.surveys.create(title: "Monkey Post")
+20.times do
+  q1 = firstpost.questions.create(question_text: "The answer is A")
+  q2 = firstpost.questions.create(question_text: "The answer is B")
+  q3 = firstpost.questions.create(question_text: "The answer is Dr. Who")
 
-q1 = firstpost.questions.create(question_text: "The answer is A")
-q2 = firstpost.questions.create(question_text: "The answer is B")
-q3 = firstpost.questions.create(question_text: "The answer is Dr. Who")
-
-[q1, q2, q3].each do |question|
-  [ "first answer", "answer b", "Dr. Who", "Glen" ].each_with_index do |index, text|
-    choice = question.choices.create( choice_text: text )
+  [q1, q2, q3].each do |question|
+    [ "first answer", "answer b", "Dr. Who", "Glen" ].each_with_index do |index, text|
+      choice = question.choices.create( choice_text: text )
+    end
+    Response.create( user_id: tom.id, choice_id: Choice.last.id - rand(3))
   end
-  Response.create( user_id: tom.id, choice_id: Choice.last.id - rand(3))
 end
 
 bob.completed_surveys.create(survey_id: firstpost.id)
