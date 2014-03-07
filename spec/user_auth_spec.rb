7b0bdc430879f93fe1c99f3bb5f0684a65845a82
Brittany Mazza & Ivan Sued <pair+LadyMozzarella.thefenry@devbootcamp.com>
2014-03-07 15:28:59 -0800

    Add user authentication spec page. Also, made a simple outline for rspec tests.

---
+++ spec/user_auth_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "user authentication" do
+
+end
+
+
+
+# get '/login' do
+# post '/login' do
+# get '/user/new' do
+# post '/user' do
+# get '/hello' do
+# get '/logout' do
